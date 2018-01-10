89332a3a3cc2e4869240b7348337f07445dbca5f
maksym <maksymus@gmail.com>
2018-01-10 01:19:58 -0500

    graph/tree - min tree

--- src/main/java/org/cracking/graph/package-info.java
+++ src/main/java/org/cracking/graph/package-info.java
@@ -6,44 +6,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Tree {
+    private Node root;
+
+    public Tree(Node root) {
+        this.root = root;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
     public static class Node<T>  {
-        T value;
-        Node left;
-        Node right;
+        private T value;
+        private Node left;
+        private Node right;
 
         public Node(T value, Node left, Node right) {
             this.value = value;
             this.left = left;
             this.right = right;
         }
-    }
 
-    Node root;
-}
-
-class Graph {
-    public static class Node<T> {
-        private T value;
-        private List<Node> adjacent = new ArrayList<>();
-
-        public Node(T value) {
-            this.value = value;
+        public Node getLeft() {
+            return left;
         }
 
-        private void join(Node other) {
-            if (this == other)
-                return;
-
-            for (Node node : adjacent) {
-                if (node == other)
-                    return;
-            }
-
-            adjacent.add(other);
-        }
-
-        public List<Node> getAdjacent() {
-            return adjacent;
+        public Node getRight() {
+            return right;
         }
 
         @Override
@@ -51,7 +40,9 @@ class Graph {
             return String.valueOf(value);
         }
     }
+}
 
+class Graph {
     private List<Node> nodes = new ArrayList<>();
     private boolean direct;
 
@@ -80,6 +71,36 @@ class Graph {
     public void add(Node ... nodes) {
         for (Node node : nodes) {
             add(node);
+        }
+    }
+
+    public static class Node<T> {
+        private T value;
+        private List<Node> adjacent = new ArrayList<>();
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        private void join(Node other) {
+            if (this == other)
+                return;
+
+            for (Node node : adjacent) {
+                if (node == other)
+                    return;
+            }
+
+            adjacent.add(other);
+        }
+
+        public List<Node> getAdjacent() {
+            return adjacent;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
         }
     }
 }
