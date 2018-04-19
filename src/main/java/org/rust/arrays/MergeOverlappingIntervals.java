159eff80c2d7ef72d0cc965ac3e3332086c20aea
maksymus@gmail.com <maksymus@gmail.com>
2018-04-19 17:02:35 -0400

    algorithms - merge overlapping interval - impl started

--- src/main/java/org/rust/arrays/MergeOverlappingIntervals.java
+++ src/main/java/org/rust/arrays/MergeOverlappingIntervals.java
@@ -1,5 +1,9 @@
 package org.rust.arrays;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Given a set of time intervals in any order, merge all overlapping intervals into one and output the result which
  * should have only mutually exclusive intervals. Let the intervals be represented as pairs of integers for simplicity.
@@ -9,6 +13,56 @@ package org.rust.arrays;
  */
 public class MergeOverlappingIntervals {
 	public static void main(String[] args) {
+		List<Interval> merged = merge(Arrays
+				.asList(new Interval(5, 7), new Interval(2, 4), new Interval(6, 8), new Interval(1, 3)));
+		System.out.println(merged);
+	}
 
+	public static List<Interval> merge(List<Interval> intervals) {
+		Collections.sort(intervals, (i1, i2) -> {
+			if (i1.from < i2.from)
+				return -1;
+
+			if (i1.from == i2.from)
+				return Integer.compare(i1.to, i2.to);
+
+			return 1;
+		});
+
+		Interval current = null;
+		for (Interval interval : intervals) {
+			if (current == null) {
+				current = interval;
+			}
+
+			if (current.to >= interval.from) {
+				current.to = interval.to;
+			}
+
+			// TODO finish me
+		}
+
+		return intervals;
+	}
+
+	private static class Interval {
+		private int from;
+		private int to;
+
+		public Interval(int from, int to) {
+			this.from = from;
+			this.to = to;
+		}
+
+		@Override
+		public String toString() {
+			return "{" + from + "," + to + '}';
+		}
 	}
 }
+
+/**
+ 	---------------
+ 		-------------
+      -----------------
+ */
